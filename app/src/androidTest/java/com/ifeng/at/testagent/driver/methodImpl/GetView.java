8cfc342d560d02a54e11b83919614c14b11dd1ff
liuru <578182160@qq.com>
2016-08-01 10:16:34 +0800

    delete unused import

--- app/src/androidTest/java/com/ifeng/at/testagent/driver/methodImpl/GetView.java
+++ app/src/androidTest/java/com/ifeng/at/testagent/driver/methodImpl/GetView.java
@@ -1,9 +1,6 @@
 package com.ifeng.at.testagent.driver.methodImpl;
 
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.ifeng.at.testagent.driver.RPCMethod;
